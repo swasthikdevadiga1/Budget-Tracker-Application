26210c273f4be2870e250c77673c312d1732e4aa
Swasthik Devadiga <169069459+swasthikdevadiga1@users.noreply.github.com>
2025-06-21 17:22:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,11 +13,13 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Database connection using Railway environment variables
-
-const dbUrl = process.env.DATABASE_URL;
-
-const connection = mysql.createConnection(dbUrl);
-
+const connection = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
+});
 connection.connect((err) => {
   if (err) {
     console.error('❌ DB connection failed:', err.stack);
